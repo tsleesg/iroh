809e5b1c0b933efbe4755e6da2bf3c8a869d6ca9
Kasey <klhuizinga@gmail.com>
2023-08-28 10:01:24 -0400

    force derp relay connectivity issues by turning on hole punching in derp only mode

--- iroh-net/src/magicsock/endpoint.rs
+++ iroh-net/src/magicsock/endpoint.rs
@@ -163,10 +163,10 @@ impl Endpoint {
     /// Returns the address(es) that should be used for sending the next packet.
     /// Zero, one, or both of UDP address and DERP addr may be non-zero.
     fn addr_for_send(&mut self, now: &Instant) -> (Option<SocketAddr>, Option<u16>, bool) {
-        if derp_only_mode() {
-            debug!("in `DEV_DERP_ONLY` mode, giving the DERP address as the only viable address for this endpoint");
-            return (None, self.derp_addr, false);
-        }
+        // if derp_only_mode() {
+        //     debug!("in `DEV_DERP_ONLY` mode, giving the DERP address as the only viable address for this endpoint");
+        //     return (None, self.derp_addr, false);
+        // }
         match self.best_addr {
             Some(ref best_addr) => {
                 if !self.is_best_addr_valid(*now) {
@@ -408,11 +408,11 @@ impl Endpoint {
     }
 
     async fn start_ping(&mut self, ep: SendAddr, now: Instant, purpose: DiscoPingPurpose) {
-        if derp_only_mode() {
-            // don't attempt any hole punching in derp only mode
-            warn!("in `DEV_DERP_ONLY` mode, ignoring request to start a hole punching attempt.");
-            return;
-        }
+        // if derp_only_mode() {
+        //     // don't attempt any hole punching in derp only mode
+        //     warn!("in `DEV_DERP_ONLY` mode, ignoring request to start a hole punching attempt.");
+        //     return;
+        // }
         info!("start ping to {}: {:?}", ep, purpose);
         if purpose != DiscoPingPurpose::Cli {
             if let Some(st) = self.endpoint_state.get_mut(&ep) {
